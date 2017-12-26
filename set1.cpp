9b70587b73b6bac1d6cf034d706f9954172cbb34
elavarasanselvam <34589746+elavarasanselvam@users.noreply.github.com>
2017-12-26 13:14:18 +0530

    Create set1.cpp

---
+++ set1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin>>x;
+    {
+    if(x>0)
+	cout<<"positive";
+	else if(x<0)
+	cout<<"Negative";
+	else
+	cout<<"zero";
+    }
+	return 0;
+}
